044d0fa0fd2d7b923af01cbb2560bab1b515bdef
yzernik <yzernik@gmail.com>
2015-01-15 21:30:56 -0800

    fixed project def

--- project.clj
+++ project.clj
@@ -1,8 +1,7 @@
 (defproject sicp "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :description "SICP solutions in Clojure"
+  :url "https://github.com/yzernik/sicp-clojure"
+  :license {:name "MIT" :url "http://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.6.0"]]
   :main ^:skip-aot sicp.core
   :target-path "target/%s"
