8b0a1342fd6b4375e57d440936b0dcce639b3185
yzernik <yzernik@gmail.com>
2015-01-15 21:28:00 -0800

    added ex1.4

---
+++ src/sicp/chapter1/ex1_4.clj
@@ -0,0 +1,13 @@
+;; Exercise 1.4
+
+
+;; Observe that our model of evaluation allows for combinations whose operators are
+;; compound expressions. Use this observation to describe the behavior of the
+;; following procedure:
+
+
+(defn a-plus-abs-b [a b]
+  ((if (> b 0) + -) a b))
+
+
+;; Add the absolute value of b to a.
